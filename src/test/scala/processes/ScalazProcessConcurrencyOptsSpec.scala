e7a85ac17281e4c472c5086c17b1a07c28fb2043
Vadim Bondarev <haghard84@gmail.com>
2015-03-28 14:44:11 +0300

    add mergeN example

--- src/test/scala/processes/ScalazProcessConcurrencyOptsSpec.scala
+++ src/test/scala/processes/ScalazProcessConcurrencyOptsSpec.scala
@@ -5,15 +5,18 @@ import java.util.concurrent.Executors._
 import mongo.MongoProgram.NamedThreadFactory
 import org.apache.log4j.Logger
 import org.specs2.mutable.Specification
+import scala.collection.IndexedSeq
+import scala.concurrent.SyncVar
+import scalaz.stream.Process._
 import scalaz.{ \/-, -\/, \/ }
 
 import scalaz.stream._
 import scalaz.concurrent.{ Strategy, Task }
 
-class ScalazProcessExternalBindingSpec extends Specification {
-  private val logger = Logger.getLogger("proc-binding")
-
+class ScalazProcessConcurrencyOptsSpec extends Specification {
   val P = scalaz.stream.Process
+
+  private val logger = Logger.getLogger("proc-binding")
 
   "Binding to asynchronous sources" should {
     "non-deterministic interleave of both streams through merge/either" in {
@@ -33,7 +36,7 @@ class ScalazProcessExternalBindingSpec extends Specification {
           case t: Throwable ⇒ callback(-\/(t))
         }
 
-      def externalSource(sleepTime: Int) = Process.eval(ioTask(sleepTime)).repeat
+      def externalSource(sleepTime: Int) = Process.repeatEval(ioTask(sleepTime))
 
       val l = externalSource(800)
       val r = externalSource(900)
@@ -53,6 +56,37 @@ class ScalazProcessExternalBindingSpec extends Specification {
         .run.run
 
       true should be equalTo true
+    }
+  }
+
+  "Binding to asynchronous sources" should {
+    "Merges non-deterministically processes with mergeN" in {
+      val ioPool = newFixedThreadPool(4, new NamedThreadFactory("io-worker"))
+
+      val pool = newFixedThreadPool(1, new NamedThreadFactory("Gatter"))
+      val consumerStrategy = Strategy.Executor(pool)
+
+      val range = 0 until 50
+      val sum = range.foldLeft(0)(_ + _)
+      val sync = new SyncVar[Throwable \/ IndexedSeq[Int]]
+
+      def resource(url: Int) =
+        P.eval(Task {
+          Thread.sleep(50) // simulate blocking call
+          logger.info(s"${Thread.currentThread.getName} - Get $url")
+          url
+        }(ioPool))
+
+      val source = emitAll(range) |> process1.lift(resource)
+
+      merge.mergeN(source)(consumerStrategy).fold(0) { (a, b) ⇒
+        val r = a + b
+        logger.info(s"${Thread.currentThread.getName} - current sum: $r")
+        r
+      }.runLog
+        .runAsync(sync.put)
+
+      sync.get should be equalTo (\/-(IndexedSeq(sum)))
     }
   }
 }
