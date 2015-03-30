ea2388c30eec27dc41115a752fff7e1f84036934
Vadim Bondarev <haghard84@gmail.com>
2015-03-30 21:13:21 +0300

    Improve AkkaScalazStreamsSpec

--- src/test/scala/akkaScalazStreams/AkkaScalazStreamsSpec.scala
+++ src/test/scala/akkaScalazStreams/AkkaScalazStreamsSpec.scala
@@ -7,7 +7,10 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.BeforeAndAfterEach
 import akka.testkit.{ ImplicitSender, TestKit }
 
+import scala.collection.mutable.Buffer
 import scalaz.concurrent.Task
+import scalaz.stream.Process._
+import scalaz.stream.io
 
 class AkkaScalazStreamsSpec extends TestKit(ActorSystem("Integration"))
     with WordSpecLike with MustMatchers
@@ -25,31 +28,40 @@ class AkkaScalazStreamsSpec extends TestKit(ActorSystem("Integration"))
     TestKit.shutdownActorSystem(system)
   }
 
+  //Mutable state lives there
+  def actorChannel = actor(new Act {
+    private var history = List[Int]()
+    become {
+      case x: Int ⇒
+        history = x :: history
+        sender() ! Integer.toHexString(x.hashCode())
+      case 'Entry =>
+        sender() ! history.reverse
+    }
+  })
+
   "Scalaz-Streams api integration with akka actors" must {
-    "with ask" in {
+    "with ask pattern" in {
       val P = Process
-      val range = 0 until 10
+      val range = 0 until 15
+
+      val ch = actorChannel
 
       val Source: Process[Task, Int] = for {
         p ← P.emitAll(range)
         i ← P.emit(p)
       } yield i
 
-      val actorChannel = actor(new Act {
-        private var messages = List[Int]()
-        become {
-          case x: Int ⇒
-            messages = x :: messages
-            sender() ! Integer.toHexString(x.hashCode())
-          case 'Entry ⇒ sender() ! messages.reverse
-        }
-      })
+      val results = Buffer.empty[String]
 
-      (Source through actorChannel.askChannel[Int, String] observe (scalaz.stream.io.stdOutLines))
-        .onFailure(ex ⇒ Process.eval(Task.delay(println("Error: " + ex.getMessage))))
-        .runLog.run.size must be === 10
+      (Source through ch.askChannel[Int, String] observe (scalaz.stream.io.stdOutLines) to io.fillBuffer(results))
+        .onFailure { ex ⇒ println("Error: " + ex.getMessage); P.halt }
+        .onComplete(P.eval(Task.delay(println("Complete..."))))
+        .runLog.run
 
-      actorChannel ! 'Entry
+      results.toList must be === range.map(s => Integer.toHexString(s.hashCode())).toList
+
+      ch ! 'Entry
       expectMsg(range.toList)
     }
   }
