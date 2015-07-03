6206e24141321b1cb7f3a80626ee21fb3e595222
Vadim Bondarev <haghard84@gmail.com>
2015-07-03 09:41:54 +0300

    Add case with scalaz actor in AksSpec

--- src/test/scala/streams/AskSpec.scala
+++ src/test/scala/streams/AskSpec.scala
@@ -1,13 +1,22 @@
 package streams
 
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors._
+
 import akka.actor._
+import mongo.MongoProgram.NamedThreadFactory
 import org.apache.log4j.Logger
 import akka.testkit.{ ImplicitSender, TestKit }
 import org.scalatest.{ BeforeAndAfterAll, BeforeAndAfterEach, MustMatchers, WordSpecLike }
 
+import scala.collection.mutable
 import scala.collection.mutable.Buffer
 import scala.concurrent.SyncVar
-import scalaz.concurrent.Task
+import scalaz.Alpha.U
+import scalaz.stream.async.mutable.Queue
+import scalaz.stream.{ Channel, async, Sink }
+import scalaz.{ \/-, \/ }
+import scalaz.concurrent.{ Strategy, Task }
 
 class AskSpec extends TestKit(ActorSystem("Integration"))
     with WordSpecLike with MustMatchers
@@ -41,6 +50,63 @@ class AskSpec extends TestKit(ActorSystem("Integration"))
     }
   })
 
+  sealed trait Response
+  case class GenericResponse[T](in: T, out: String) extends Response
+
+  sealed trait Request {
+    type In
+    type Out
+    def value: In
+    def cb: (Throwable \/ Out ⇒ Unit)
+  }
+
+  def envelop[T, B, C <: Request](v: T, f: Throwable \/ B ⇒ Unit) = new Request {
+    override type In = T
+    override type Out = B
+    override def value: In = v
+    override def cb: Throwable \/ Out ⇒ Unit = f
+  }
+
+  import scalaz.concurrent.{ Actor ⇒ ActorZ }
+  val S = newFixedThreadPool(3, new NamedThreadFactory("actors"))
+
+  def scalazActor = ActorZ[Request] {
+    case e: Request ⇒ {
+      e.cb(\/-(GenericResponse(e.value,
+        s"${Thread.currentThread().getName}: Hello from type message ${e.value.getClass.getName}").asInstanceOf[e.Out]))
+    }
+    case other ⇒ ()
+  }(Strategy.Executor(S))
+
+  implicit class ScalazActorRefSyntax[A <: Request](val self: scalaz.concurrent.Actor[A]) {
+    def scalazChannel[T, B]: Channel[Task, T, B] = {
+      scalaz.stream.channel.lift[Task, T, B] { message: T ⇒
+        Task.async { cb: (\/[Throwable, B] ⇒ Unit) ⇒ self ! envelop(message, cb).asInstanceOf[A] }
+      }
+    }
+  }
+
+  "Process through scalaz actors" must {
+    "request/response" in {
+      val latch = new CountDownLatch(2)
+      val source: Process[Task, Double] = P.emitAll(Seq(0.1, 0.67, 0.35, 0.68, 0.11, 8, 98.89, 9, 3.4, 67.89, 5.12, 90.45))
+      val source2: Process[Task, Int] = P.emitAll(Seq.range(1, 11))
+
+      Task.fork {
+        (source through scalazActor.scalazChannel[Double, Response]).map(_.toString)
+          .to(scalaz.stream.io.stdOutLines).runLog
+      }(S).runAsync(_ ⇒ latch.countDown())
+
+      Task.fork {
+        (source2 through scalazActor.scalazChannel[Int, Response]).map(_.toString)
+          .to(scalaz.stream.io.stdOutLines).runLog
+      }(S).runAsync(_ ⇒ latch.countDown())
+
+      latch.await()
+      1 === 1
+    }
+  }
+
   "Scalaz-Streams integration with akka actors" must {
     "with ask pattern" in {
       val range = 0 until 15
@@ -52,9 +118,9 @@ class AskSpec extends TestKit(ActorSystem("Integration"))
         i ← P.emit(p)
       } yield i
 
-      val results = Buffer.empty[String]
+      val results = mutable.Buffer.empty[String]
 
-      (Source through ch.akkaChannel[Int, String]() observe (scalaz.stream.io.stdOutLines) to scalaz.stream.io.fillBuffer(results))
+      (Source through ch.akkaChannel[Int, String]() observe scalaz.stream.io.stdOutLines to scalaz.stream.io.fillBuffer(results))
         .onFailure { ex ⇒ println("Error: " + ex.getMessage); P.halt }
         .onComplete(P.eval(Task.delay(println("Complete..."))))
         .runLog.run
--- src/test/scala/streams/intergation/IntegrationSpec.scala
+++ src/test/scala/streams/intergation/IntegrationSpec.scala
@@ -225,7 +225,7 @@ class IntegrationSpec extends TestKit(ActorSystem("integration"))
       val flow = Flow[Int].map(_ * 2)
         .withAttributes(OperationAttributes.inputBuffer(initial = size * 2, max = size * 2))
 
-      (source.throughAkkaFlow(size, flow)).fold1(_ ++ _)
+      source.throughAkkaFlow(size, flow).fold1(_ ++ _)
         .runLog.run must be === Vector(range.toVector.map(_ * 2))
     }
   }
