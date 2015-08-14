a7f4a281867244197621242bda8da84bd25f9815
Vadim Bondarev <haghard84@gmail.com>
2015-08-14 12:58:37 +0300

    Add MergeSort example

--- src/test/scala/processes/GeneralsSpec.scala
+++ src/test/scala/processes/GeneralsSpec.scala
@@ -1,5 +1,6 @@
 package processes
 
+import org.scalacheck.Gen
 import org.specs2.mutable.Specification
 
 import scala.collection.mutable
@@ -33,6 +34,39 @@ class GeneralsSpec extends Specification {
 
       intOut should be equalTo a
       strOut should be equalTo b
+    }
+  }
+
+  def mergeSorted[T: Ordering](left: List[T], right: List[T])(implicit ord: Ordering[T]): List[T] = {
+    val source0 = Process.emitAll(left)
+    val source1 = Process.emitAll(right)
+
+    def choose(l: T, r: T): Tee[T, T, T] =
+      if (ord.lt(l, r)) Process.emit(l) ++ nextL(r)
+      else Process.emit(r) ++ nextR(l)
+
+    def nextR(l: T): Tee[T, T, T] =
+      tee.receiveROr[T, T, T](Process.emit(l) ++ tee.passL)(choose(l, _))
+
+    def nextL(r: T): Tee[T, T, T] =
+      tee.receiveLOr[T, T, T](Process.emit(r) ++ tee.passR)(choose(_, r))
+
+    def init: Tee[T, T, T] =
+      tee.receiveLOr[T, T, T](tee.passR)(nextR)
+
+    (source0 tee source1)(init).toSource.runLog.run.toList
+  }
+
+  "MergeSort deterministically 2 sorted list" should {
+    "run" in {
+      val left = Gen.listOf(Gen.choose(0, 20)).map(_.sorted).sample.getOrElse(Nil)
+      val right = Gen.listOf(Gen.choose(0, 20)).map(_.sorted).sample.getOrElse(Nil)
+      println(left)
+      println(right)
+
+      val result = mergeSorted(left, right)
+      println(result)
+      result should be equalTo (left ::: right).sorted
     }
   }
 }
