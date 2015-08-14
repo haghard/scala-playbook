5c957f2d20824437e5b77fe4429e35382e0fca3e
Vadim Bondarev <haghard84@gmail.com>
2015-08-14 14:00:31 +0300

    Cleanup

--- src/test/scala/processes/GeneralsSpec.scala
+++ src/test/scala/processes/GeneralsSpec.scala
@@ -6,26 +6,25 @@ import org.specs2.mutable.Specification
 import scala.collection.mutable
 import scalaz.stream.Process
 import scalaz.stream._
+import Process._
 
 class GeneralsSpec extends Specification {
-
-  val P = Process
 
   "Writer" should {
     "write" in {
       val strOut = mutable.Buffer[String]()
       val intOut = mutable.Buffer[Int]()
-      val naturals = P.emitAll(1 to 10)
+      val naturals = emitAll(1 to 10)
 
       (for {
         o ← naturals.take(2)
-        r ← (P.emitW("Incoming value: " + o) ++ P.emitO(o))
+        r ← (emitW("Incoming value: " + o) ++ emitO(o))
           .drainW(io.fillBuffer(strOut))
       } yield (r)).runLog.run
 
       (for {
         o ← naturals.take(5)
-        r ← (P.emitW("Incoming value: " + o) ++ P.emitO(o))
+        r ← (emitW("Incoming value: " + o) ++ emitO(o))
           .drainO(io.fillBuffer(intOut))
       } yield (r)).runLog.run
 
@@ -38,18 +37,18 @@ class GeneralsSpec extends Specification {
   }
 
   def mergeSorted[T: Ordering](left: List[T], right: List[T])(implicit ord: Ordering[T]): List[T] = {
-    val source0 = Process.emitAll(left)
-    val source1 = Process.emitAll(right)
+    val source0 = emitAll(left)
+    val source1 = emitAll(right)
 
     def choose(l: T, r: T): Tee[T, T, T] =
-      if (ord.lt(l, r)) Process.emit(l) ++ nextL(r)
-      else Process.emit(r) ++ nextR(l)
+      if (ord.lt(l, r)) emit(l) ++ nextL(r)
+      else emit(r) ++ nextR(l)
 
     def nextR(l: T): Tee[T, T, T] =
-      tee.receiveROr[T, T, T](Process.emit(l) ++ tee.passL)(choose(l, _))
+      tee.receiveROr[T, T, T](emit(l) ++ tee.passL)(r ⇒ choose(l, r))
 
     def nextL(r: T): Tee[T, T, T] =
-      tee.receiveLOr[T, T, T](Process.emit(r) ++ tee.passR)(choose(_, r))
+      tee.receiveLOr[T, T, T](emit(r) ++ tee.passR)(l ⇒ choose(l, r))
 
     def init: Tee[T, T, T] =
       tee.receiveLOr[T, T, T](tee.passR)(nextR)
@@ -59,8 +58,8 @@ class GeneralsSpec extends Specification {
 
   "MergeSort deterministically 2 sorted list" should {
     "run" in {
-      val left = Gen.listOf(Gen.choose(0, 20)).map(_.sorted).sample.getOrElse(Nil)
-      val right = Gen.listOf(Gen.choose(0, 20)).map(_.sorted).sample.getOrElse(Nil)
+      val left = Gen.listOf(Gen.choose(0, 50)).map(_.sorted).sample.getOrElse(Nil)
+      val right = Gen.listOf(Gen.choose(0, 50)).map(_.sorted).sample.getOrElse(Nil)
       println(left)
       println(right)
 
