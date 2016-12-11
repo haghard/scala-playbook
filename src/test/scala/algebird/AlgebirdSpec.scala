c5a16eea97ac5d6a52e89eb4460d948a29e38695
Vadim Bondarev <haghard84@gmail.com>
2016-12-12 00:17:07 +0300

    update algebird

--- src/test/scala/algebird/AlgebirdSpec.scala
+++ src/test/scala/algebird/AlgebirdSpec.scala
@@ -24,12 +24,15 @@ class AlgebirdSpec extends Specification {
     mh.similarity(sigX, sigY)
   }
 
-  "Hasher similarity" should {
+  //MinHash gives approximate set similarity. Works only for sets
+
+  "MinHash similarity" should {
     "run" in {
       val x = "qwerty".toSet
       val y = "qwerty1".toSet
 
-      val hasher = new com.twitter.algebird.MinHasher32(1.0, 1024)
+      import com.twitter.algebird._
+      val hasher = new MinHasher32(1.0, 1024) //size 1 kb
 
       val sig0 = x.map(elem ⇒ hasher.init(elem.toString)).reduce(hasher.plus(_, _))
       val sig1 = y.map(elem ⇒ hasher.init(elem.toString)).reduce(hasher.plus(_, _))
@@ -41,6 +44,53 @@ class AlgebirdSpec extends Specification {
       //hs === approxSimilarity(hasher, x, y)
       //hs === s
       1 === 1
+    }
+  }
+
+  "MinHash " should {
+    "Use signatures and buckets to find similar sets" in {
+      import com.twitter.algebird._
+      //targetThreshold
+      //0.005 - 247 buckets number
+      //0.009 - 224
+      //0.01 - 123
+      //0.02 - 108
+
+      /*
+        Sets whose signatures
+        end up in the same bucket are likely to be similar. The targetThreshold controls
+        the desired level of similarity - the higher the threshold, the more efficiently
+        you can find all the similar sets.
+      */
+
+      val h = new MinHasher32(0.005, 1 << 10) //1024 bytes
+
+      val a = Set("b","c","d","e","f","g","h")
+      val b = Set("b","c","d","e","f","g","l","a")
+      val c = Set("a","c","d","e","f","g","l","h","y")
+      val d = Set("c","a","e","d","g","f","l","h")
+
+      //1 kb signature a representation of followers
+      val as = a.map(h.init(_)).reduce(h.plus(_, _))
+      val bs = b.map(h.init(_)).reduce(h.plus(_, _))
+      val cs = c.map(h.init(_)).reduce(h.plus(_, _))
+      val ds = d.map(h.init(_)).reduce(h.plus(_, _))
+
+      //hasher.similarity(as, ds)
+      //hasher.similarity(cs, ds)
+
+      //seq based on buckets
+      val aBucSeq = h.buckets(as).map(h.init(_)).reduce(h.plus(_, _))
+      val bBucSeq = h.buckets(bs).map(h.init(_)).reduce(h.plus(_, _))
+      val cBucSeq = h.buckets(cs).map(h.init(_)).reduce(h.plus(_, _))
+      val dBucSeq = h.buckets(ds).map(h.init(_)).reduce(h.plus(_, _))
+
+      //to avoid n^2 comparisons for all item now we can group items by buckets and compare them inside single bucket and
+      //to get n^2 comparisons inside each bucket.
+      //because with very high probability similar items should end up at the same bucket
+
+      // c more similar to d than a similar to d
+      h.similarity(aBucSeq, bBucSeq) < h.similarity(cBucSeq, dBucSeq)
     }
   }
 
@@ -69,7 +119,7 @@ class AlgebirdSpec extends Specification {
     }
   }
 
-  "BloomFilter" should {
+  "BloomFilter for group membership" should {
     "run" in {
       import com.twitter.algebird._
       val NUM_HASHES = 6
